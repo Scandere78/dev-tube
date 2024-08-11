258906daa65971ecd7f9a31f8ebd6c36e6f5a944
ScandereT78 <scanderetejpro@gmail.com>
2024-08-11 16:32:18 +0100

    add navbar logo

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+  <header class="m-8 mt-4 rounded-xl flex justify-center bg-white shadow-sm">
+    <nav class="w-full mx-12 flex flex-row justify-between items-center">
+      <RouterLink to="/" class="w-10">
+        <img src="/devtube.png" alt="Devtube logo">
+      </RouterLink>
+      <form>
+        <input type="text" placeholder="Search..." class="my-3 ml-8 px-4 py-3 rounded-xl bg-gray-100 text-sm">
+      </form>
+      <font-awesome-icon icon="magnifying-glass" />
+    </nav>
+  </header>
   <RouterView />
 </template>
 
@@ -8,13 +19,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
