200e098dc5e1f073adcecbc9b763c30bd630b220
ScandereT78 <scanderetejpro@gmail.com>
2024-08-11 11:51:22 +0100

    start the project dev-tube probleme to the root not affiche in the browser Home and Video

---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+import HomeFeed from "../views/HomeFeed.vue";
+import VideoDetails from "../views/VideoDetails.vue";
+import { createWebHashHistory } from "vue-router";
+import { createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: "Home",
+        component:HomeFeed
+    },
+    {
+        path:'/video:id',
+        name: "Video",
+        component:VideoDetails
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory,
+    routes
+})
+export default router
