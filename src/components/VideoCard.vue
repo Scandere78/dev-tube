3b11b33c26c357564f400b4baae0e7b152b31da7
ScandereT78 <scanderetejpro@gmail.com>
2024-09-06 22:23:05 +0100

    affichqge d'information comme le nombre de vue et la date de publication

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <img :src="videoInfo.video.thumbnails[0].url" alt="">
-        <h2>{{ videoInfo.video.title }}</h2>
+        <h3>{{ videoInfo.video.title }}</h3>
+        <h4>DEVTUBE</h4>
+        <p>
+            {{videoInfo.video.stats.views}} views -
+            {{ videoInfo.video.publishedTimeText }}</p>
     </div>
 </template>
 
